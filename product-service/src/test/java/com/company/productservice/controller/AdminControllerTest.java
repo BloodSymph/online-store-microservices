5c7cdf1e8fa151c4d3efaa653b54f69576c8f197
BloodSymph <rubakharoma@gmail.com>
2023-12-19 23:19:41 +0200

    Product service test is finished!

--- product-service/src/test/java/com/company/productservice/controller/AdminControllerTest.java
+++ product-service/src/test/java/com/company/productservice/controller/AdminControllerTest.java
@@ -8,16 +8,22 @@ import com.company.productservice.dto.category.CategoryResponse;
 import com.company.productservice.dto.product.ProductAdminResponse;
 import com.company.productservice.dto.product.ProductRequest;
 import com.company.productservice.service.AdminService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.*;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
@@ -38,6 +44,9 @@ class AdminControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private AdminService adminService;
 
@@ -53,7 +62,6 @@ class AdminControllerTest {
 
     private ProductRequest productRequest;
 
-    private Pageable pageable;
 
     @BeforeEach
     void setUp() {
@@ -73,52 +81,177 @@ class AdminControllerTest {
                 .url("category")
                 .build();
 
-        pageable = PageRequest.of(
-                0, 10,
-                Sort.by(
-                        Sort.Direction.ASC, "name"
+        brandAdminResponse = BrandAdminResponse.builder()
+                .id(1L)
+                .version(1L)
+                .name("Brand")
+                .url("brand")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        brandRequest = BrandRequest.builder()
+                .version(1L)
+                .name("Brand")
+                .url("brand")
+                .build();
+
+        productAdminResponse = ProductAdminResponse.builder()
+                .id(1L)
+                .version(1L)
+                .name("Product")
+                .url("product")
+                .price("100$")
+                .photoUrl("https://photo.com")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        productRequest = ProductRequest.builder()
+                .version(1L)
+                .name("Product")
+                .url("product")
+                .price("100$")
+                .photoUrl("https://photo.com")
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("Create category test!")
+    void AdminController_CreateCategory_ReturnCategoryAdminResponse() throws Exception {
+
+        Mockito.lenient().when(
+                adminService.createCategory(categoryRequest)
+        ).thenReturn(categoryAdminResponse);
+
+        mockMvc.perform(
+                post(
+                        "/api/v1/product-service/admin/categories/create"
+                ).contentType(MediaType.APPLICATION_JSON).content(
+                        objectMapper.writeValueAsString(categoryRequest)
                 )
-        );
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        ).andDo(print());
 
     }
 
     @Test
-    void createCategory() {
+    @DisplayName("Update category test!")
+    void AdminController_UpdateCategory_ReturnCategoryAdminResponse() throws Exception {
+
+        Mockito.lenient().when(
+                adminService.updateCategory(
+                        categoryRequest,
+                        "category"
+                )
+        ).thenReturn(categoryAdminResponse);
+
+        mockMvc.perform(
+                put(
+                        "/api/v1/product-service/admin/categories/category/update"
+                ).contentType(MediaType.APPLICATION_JSON).content(
+                        objectMapper.writeValueAsString(categoryRequest)
+                )
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        ).andDo(print());
 
     }
 
     @Test
-    void updateCategory() {
+    @DisplayName("Create brand test!")
+    void AdminController_CreateBrand_ReturnBrandAdminResponse() throws Exception {
+
+        Mockito.lenient().when(
+            adminService.createBrand(
+                  brandRequest,
+                  "category"
+            )
+        ).thenReturn(brandAdminResponse);
+
+        mockMvc.perform(
+                post(
+                        "/api/v1/product-service/admin/categories/category/brands/create"
+                ).contentType(MediaType.APPLICATION_JSON).content(
+                        objectMapper.writeValueAsString(brandRequest)
+                )
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        ).andDo(print());
 
     }
 
     @Test
-    void createBrand() {
+    @DisplayName("Update brand test!")
+    void AdminController_UpdateBrand_ReturnBrandAdminResponse() throws Exception {
+
+        Mockito.lenient().when(
+                adminService.updateBrand(
+                        brandRequest,
+                        "brand"
+                )
+        ).thenReturn(brandAdminResponse);
+
+        mockMvc.perform(
+                put(
+                        "/api/v1/product-service/admin/brands/brand/update"
+                ).contentType(MediaType.APPLICATION_JSON).content(
+                        objectMapper.writeValueAsString(brandRequest)
+                )
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        ).andDo(print());
+
     }
 
     @Test
-    void updateBrand() {
+    @DisplayName("Create product test!")
+    void AdminController_CreateProduct_ReturnProductAdminResponse() throws Exception {
+
+        Mockito.lenient().when(
+                adminService.createProduct(
+                        productRequest,
+                        "category",
+                        "brand"
+                )
+        ).thenReturn(productAdminResponse);
+
+        mockMvc.perform(
+                post(
+                        "/api/v1/product-service/admin/categories/category/brands/brand/products/create"
+                ).contentType(MediaType.APPLICATION_JSON).content(
+                        objectMapper.writeValueAsString(productRequest)
+                )
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        ).andDo(print());
+
     }
 
     @Test
-    void createProduct() {
+    @DisplayName("Update product test!")
+    void AdminController_UpdateProduct_ReturnProductAdminResponse() throws Exception {
+
+        Mockito.lenient().when(
+                adminService.updateProduct(
+                        productRequest,
+                        "product"
+                )
+        ).thenReturn(productAdminResponse);
+
+        mockMvc.perform(
+                put(
+                        "/api/v1/product-service/admin/products/product/update"
+                ).contentType(MediaType.APPLICATION_JSON).content(
+                        objectMapper.writeValueAsString(productRequest)
+                )
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        ).andDo(print());
+
     }
 
-    @Test
-    void updateProduct() {
-    }
-
-
-    @Test
-    void createProductInformation() {
-    }
-
-    @Test
-    void updateProductInformation() {
-    }
-
-    @Test
-    void deleteProductInformation() {
-    }
 
 }
